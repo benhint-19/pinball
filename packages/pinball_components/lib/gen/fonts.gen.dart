07861bca9b273ead72020d2105f3526d74d0d42d
benhint-19 <benfdhinton@gmail.com>
2026-02-15 12:59:54 -0700

    move to firebase

--- packages/pinball_components/lib/gen/fonts.gen.dart
+++ packages/pinball_components/lib/gen/fonts.gen.dart
@@ -1,9 +1,12 @@
+// dart format width=80
 /// GENERATED CODE - DO NOT MODIFY BY HAND
 /// *****************************************************
 ///  FlutterGen
 /// *****************************************************
 
-// ignore_for_file: directives_ordering,unnecessary_import
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
 
 class FontFamily {
   FontFamily._();
